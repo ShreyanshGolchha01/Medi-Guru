2cb72d749ced0c5ba585b2713176ad5c4ef0001d
Shreyansh Golchha <shreyansh.golchha2023@ssipmt.com>
2025-08-03 23:59:21 +0530

    Update server.tsx

--- src/services/server.tsx
+++ src/services/server.tsx
@@ -1,3 +1,4 @@
-const serverUrl = 'http://165.22.208.62:8080/mediguru_api/';
+// const serverUrl = 'http://165.22.208.62:8080/mediguru_api/';
+const serverUrl = 'http://localhost/mediguru/';
 
 export default serverUrl;
